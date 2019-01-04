c366f6324053b5a136d2a3cebf98eefbac35fe56
Dani-ella <46262161+Dani-ella@users.noreply.github.com>
2019-01-03 23:22:02 -0800

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,5 @@
+#This code allows user to input name and then receive an output display of greetings including user's name.
+
+name = input("\n Hi please tell us your name\n") # allows user input   
+
+print("\n Hello " + name + " Nice to meet you.") # displays greeting including name input received.
